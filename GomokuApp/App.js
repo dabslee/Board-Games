bdfba5b43b0074e53d025473f193fd96c26f29a8
Brandon Lee <brandon.sangmin.lee@gmail.com>
2025-12-30 14:25:59 -0500

    Moved gitignore, fixed iOS versioning issues

--- GomokuApp/App.js
+++ GomokuApp/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GameProvider } from './src/context/GameContext';
 import StartScreen from './src/screens/StartScreen';
 import GameScreen from './src/screens/GameScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
